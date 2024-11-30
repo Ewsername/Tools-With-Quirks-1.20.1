3d3c728a12037619122e9ea03e489cb43022b2f5
Ewsername <159178256+Ewsername@users.noreply.github.com>
2024-11-30 01:02:27 +0100

    Initial commit

---
+++ src/main/java/net/bunmuppet/toolswithquirks/ToolsWithQuirks.java
@@ -0,0 +1,17 @@
+package net.bunmuppet.toolswithquirks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ToolsWithQuirks implements ModInitializer {
+
+	public static final String MOD_ID = "tools-with-quirks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bunmuppet/toolswithquirks/ToolsWithQuirksClient.java
@@ -0,0 +1,11 @@
+package net.bunmuppet.toolswithquirks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToolsWithQuirksClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
